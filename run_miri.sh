01c4e642437694d3ceb8c5aae941847528f52d9e
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2020-09-21 16:26:23 +0200

    Update to the new way Miri works
    Fixes #1

--- run_miri.sh
+++ run_miri.sh
@@ -6,7 +6,5 @@ rustup set profile minimal
 rustup default "$MIRI_NIGHTLY"
 
 rustup component add miri
-cargo miri setup
 
-cargo miri test -- -- -Zunstable-options --exclude-should-panic
-
+cargo miri test
